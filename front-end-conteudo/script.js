7aed0f8ad1810a65dc62e1af8f91a89c692eeb88
Raul Ramos <raulramosneves9@gmail.com>
2026-03-12 16:47:33 -0300

    PUT and GET adicionado

--- front-end-conteudo/script.js
+++ front-end-conteudo/script.js
@@ -3,6 +3,7 @@ const userAge = document.getElementById('inputAge');
 const userEmail = document.getElementById('inputEmail');
 const createButton = document.getElementById('createButton');
 
+// Função para CRIAR um novo usuário
 async function createUser() {
     const response = await fetch("http://localhost:8000/api/users", {
         method: "POST",
@@ -19,3 +20,31 @@ async function createUser() {
 }
 
 createButton.addEventListener('click', createUser); // ← conecta botão à função
+
+// Função para ATUALIZAR um usuário, POR COMPLETO
+async function updateUser() {
+    const response = await fetch("http://localhost:8000/api/users?index=0", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            name: "Victor Atualizado",
+            age: 25,
+            email: "novo@email.com"
+        }),
+    });
+    const updated = await response.json();
+    console.log(updated);
+}
+
+// Função para GET a lista de usuários
+async function getUsers() {
+    const response = await fetch("http://localhost:8000/api/users");
+    const users = await response.json();
+    console.log(users);
+}
+
+// Função para DELETAR um usuário
+const response = await fetch("http://localhost:8000/api/users?index=0", {
+    method: "DELETE",
+});
+const result = await response.json();
