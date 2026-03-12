82a9349adc7f128ece051efac3b6b0fa5e3ca8e1
Raul Ramos <raulramosneves9@gmail.com>
2026-03-12 17:02:16 -0300

    adicionando pasta scripts, styles, dockers

--- src/scripts/api/created.js
+++ src/scripts/api/created.js
@@ -43,8 +43,3 @@ async function getUsers() {
     console.log(users);
 }
 
-// Função para DELETAR um usuário
-const response = await fetch("http://localhost:8000/api/users?index=0", {
-    method: "DELETE",
-});
-const result = await response.json();
